71f3d194666f4ff7becd7b9ee8f333043f152d58
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2017-12-19 10:35:04 +0100

    Added WILT

--- items/items.js
+++ items/items.js
@@ -5,6 +5,7 @@
 //     hidden <- Optional: whether you want it to not appear listed at home
 // );
 
+addItem("Monday", "2017-12-18", "18/12/2017");
 addItem("Now", "now", "7/12/2017");
 addItem("App screen creation template", "app-screen-creation-template", "17/2/2017", false, "Xamarin");
 addItem("Conferences and talks", "conferences", "7/12/2017");
