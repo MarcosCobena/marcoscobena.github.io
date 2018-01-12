fae026478b929138e8e8cf4652e26d0ab9fe3c44
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-01-12 09:52:28 +0100

    Added WILT

--- items/items.js
+++ items/items.js
@@ -5,6 +5,7 @@
 //     hidden <- Optional: whether you want it to not appear listed at home
 // );
 
+addItem("Thursday", "2018-1-11", "11/1/2018");
 addItem("Wednesday", "2018-1-10", "10/1/2018");
 addItem("Tuesday", "2018-1-9", "9/1/2018");
 addItem("Monday", "2018-1-8", "8/1/2018");
