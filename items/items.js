6c73cd5e55138d6d4c943fa3f3961d8e8ef4cd3d
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-02-01 13:34:27 +0100

    Added WILT

--- items/items.js
+++ items/items.js
@@ -1,4 +1,5 @@
-﻿addItem("Tuesday", "2018-1-30", "30/1/2018", false, "Xamarin");
+addItem("Tools", "Tools", "1/2/2018", false, "Xamarin");
+addItem("Tuesday", "2018-1-30", "30/1/2018", false, "Xamarin");
 addItem("Monday", "2018-1-29", "29/1/2018", false, "Xamarin");
 addItem("Saturday", "2018-1-27", "27/1/2018", false, "Xamarin");
 addItem("Friday", "2018-1-26", "26/1/2018", false, "Xamarin");
