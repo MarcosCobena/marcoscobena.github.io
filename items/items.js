0efb86e3f6afaad74b8e48934895e4c5edd07028
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-05-08 23:28:07 +0200

    Added drones

--- items/items.js
+++ items/items.js
@@ -7,6 +7,7 @@
 
 addItem("Home", "home", "17/3/2018");
 addItem("Conferences", "conferences", "7/12/2017");
+addItem("FPV Drones", "drones", "3/5/2018");
 addItem("Reading", "reading", "19/4/2018");
 addItem("Tools", "tools", "5/3/2018", false, "Xamarin");
 addItem("🤷", "404", "15/2/2018", true);
