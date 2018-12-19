69b66498ff7d7255b2091a98a1458b44cfcb84b6
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-12-19 16:15:56 +0100

    Updated portfolio

--- items/items.js
+++ items/items.js
@@ -15,7 +15,7 @@ addItem("Contact", "contact", "22/8/2018");
 addItem("FPV Drones", "drones", "8/5/2018");
 addItem("Git", "git", "20/9/2018");
 addItem("Key-bindings", "key-bindings", "24/5/2018");
-addItem("Portfolio", "portfolio", "4/10/2018");
+addItem("Portfolio", "portfolio", "19/12/2018");
 addItem("Questions", "questions", "7/5/2018");
 addItem("Reading", "reading", "3/12/2018");
 addItem("Recipes", "recipes", "14/6/2018");
