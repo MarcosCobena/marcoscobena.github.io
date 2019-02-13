542a429efe119ab387eb17badab3eef4f88786c2
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-02-13 19:41:00 +0100

    Updated samples

--- tmp/webgldotnet/uno-config.js
+++ tmp/webgldotnet/uno-config.js
@@ -1,4 +1,4 @@
-config.uno_remote_managedpath = "managed-2dd4f187ceb1f372c85e49d09282faf3c91eeb17";
+config.uno_remote_managedpath = "managed-5f26a592ecc9291db6883cdc374970d9d823ec00";
 config.uno_dependencies = [];
 config.uno_main = "[Samples] Samples.Program:Main";
 config.assemblyFileExtension = "clr";
