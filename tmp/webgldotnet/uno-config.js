642b0aae6571c1fe17606858e05d77802652b18f
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-03-15 13:49:21 +0100

    Updated WebGL.NET

--- tmp/webgldotnet/uno-config.js
+++ tmp/webgldotnet/uno-config.js
@@ -1,4 +1,4 @@
-config.uno_remote_managedpath = "managed-e6107051edd23e2018b7953a8385d47de4841dbd";
+config.uno_remote_managedpath = "managed-0102e59c76fb9736fc1409302bedba4725e018c8";
 config.uno_dependencies = [];
 config.uno_main = "[Samples] Samples.Program:Main";
 config.assemblyFileExtension = "clr";
