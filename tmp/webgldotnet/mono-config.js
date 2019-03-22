653b38127ea7f42fa48f5a2551893b71d42650f0
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-03-22 13:05:15 +0100

    Updated WebGL.NET

--- tmp/webgldotnet/mono-config.js
+++ tmp/webgldotnet/mono-config.js
@@ -2,6 +2,6 @@ config = {
  	vfs_prefix: "managed",
  	deploy_prefix: "managed",
  	enable_debugging: 0,
- 	file_list: [ "Samples.dll","mscorlib.dll","System.Xml.dll","System.Drawing.dll","System.Runtime.Serialization.dll","WebAssembly.Bindings.dll","WebGLDotNET.dll","WaveEngine.Common.dll", ],
+ 	file_list: [ "Samples.dll","mscorlib.dll","System.Xml.dll","System.Drawing.dll","System.Runtime.Serialization.dll","WebAssembly.Bindings.dll","WaveEngine.Common.dll","WebGLDotNET.dll", ],
 	add_bindings: function() { Module.mono_bindings_init ("[WebAssembly.Bindings]WebAssembly.Runtime"); }
 }
--- tmp/webgldotnet/uno-config.js
+++ tmp/webgldotnet/uno-config.js
@@ -1,4 +1,4 @@
-config.uno_remote_managedpath = "managed-0102e59c76fb9736fc1409302bedba4725e018c8";
+config.uno_remote_managedpath = "managed-12292007352afaacbe3890c3dbe455232e0d7772";
 config.uno_dependencies = [];
 config.uno_main = "[Samples] Samples.Program:Main";
 config.assemblyFileExtension = "clr";
