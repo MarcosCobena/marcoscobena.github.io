7bebb6f659468cce3bbd254a3c9676c5a63f120b
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-04-01 16:47:46 +0200

    Updated WebGL.NET

--- tmp/webgldotnet/mono-config.js
+++ tmp/webgldotnet/mono-config.js
@@ -2,6 +2,5 @@ config = {
  	vfs_prefix: "managed",
  	deploy_prefix: "managed",
  	enable_debugging: 0,
- 	file_list: [ "Samples.dll","mscorlib.dll","System.Xml.dll","System.Drawing.dll","System.Runtime.Serialization.dll","WebAssembly.Bindings.dll","WaveEngine.Common.dll","WebGLDotNET.dll", ],
-	add_bindings: function() { Module.mono_bindings_init ("[WebAssembly.Bindings]WebAssembly.Runtime"); }
+ 	file_list: [ "Samples.dll","netstandard.dll","mscorlib.dll","System.Core.dll","System.dll","Mono.Security.dll","System.Xml.dll","System.Numerics.dll","System.Data.dll","System.Transactions.dll","System.Drawing.Common.dll","System.IO.Compression.dll","System.IO.Compression.FileSystem.dll","System.ComponentModel.Composition.dll","System.Net.Http.dll","System.Runtime.Serialization.dll","System.ServiceModel.Internals.dll","System.Web.Services.dll","System.Xml.Linq.dll","WebGLDotNET.dll","WebAssembly.Bindings.dll","System.Memory.dll","WaveEngine.Common.dll","protobuf-net.dll","WebAssembly.Net.Http.dll","WebAssembly.Net.WebSockets.dll" ],
 }
