4e1d96be0a17f528e05d159a190d46ce0ff1c48a
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2020-03-25 22:07:45 +0100

    Updated home's date

--- items/items.js
+++ items/items.js
@@ -8,7 +8,7 @@
 // Items —it doesn't mind the order how they're added
 
 addItem("404 Not Found", "404", "29/8/2018");
-addItem("👋 ¡Hola!", "home", "1/10/2019");
+addItem("👋 ¡Hola!", "home", "25/3/2020");
 addItem("Audios", "audios", "28/5/2018");
 addItem("Blog", "blog", "22/8/2018");
 addItem("Conferences", "conferences", "30/7/2019");
