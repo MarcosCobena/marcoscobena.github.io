8cd57e46edcbf9d7fe5ca42fa000cb4b601839d3
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2021-01-23 20:15:51 +0100

    fix PR comment

--- items/home.js
+++ items/home.js
@@ -1,2 +1,2 @@
-const posts = items.filter(item => item.tags.some(tag => tag == blogTag));
+const posts = items.filter(item => item.tags.some(tag => tag == blogTag) && !item.tags.some(tag => tag == 'es'));
 listItems('#posts-latest', posts, 'blog', 5);
