ebf9052db30c4ab95a4b91aadd3d32308b191d8f
Marcos Cobeña Morián <marcoscobena@outlook.com>
2025-06-22 13:18:29 +0200

    update blog to show selected tag (if any)

--- items/blog.js
+++ items/blog.js
@@ -3,5 +3,11 @@ const queryString = new URLSearchParams(window.location.search);
 const tag = queryString.has('t')
     ? queryString.get('t')
     : blogTag;
+
+if (tag != blogTag) {
+    const selectedTagElement = document.querySelector('#tag-selected');
+    selectedTagElement.innerHTML = `Posts with tag <em>${tag}</em>:`;
+}
+
 const posts = items.filter(item => item.tags.some(eachTag => eachTag == tag));
 listItems('#posts', posts);
--- items/blog.md
+++ items/blog.md
@@ -3,4 +3,6 @@
 Tags:
 <span id="tags"></span>
 
+<div id="tag-selected"></div>
+
 <div id="posts"></div>
